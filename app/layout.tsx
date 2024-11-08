b18d1ef136c633e0847a2cf2dc2881e4b230cccb
RezaRahimi166 <rezaa.salami99@gmail.com>
2024-11-08 14:00:53 -0900

    feat(patient): creating patient form and custom shadcn formfiled

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,6 @@ export default function RootLayout({
         <ThemeProvider attribute="class" defaultTheme="dark">
           {children}
         </ThemeProvider>
-        {children}
       </body>
     </html>
   );
--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -1,6 +1,7 @@
 "use server";
 import { ID, Query } from "node-appwrite";
 import { users } from "../appwrite.config";
+import { parseStringify } from "../utils";
 
 export const createUser = async (user: CreateUserParams) => {
   try {
@@ -20,5 +21,14 @@ export const createUser = async (user: CreateUserParams) => {
 
       return existingUser?.users[0];
     }
+  }
+};
+
+export const getUser = async (userId: string) => {
+  try {
+    const user = await users.get(userId);
+    return parseStringify(user);
+  } catch (error) {
+    console.log(error);
   }
 };
